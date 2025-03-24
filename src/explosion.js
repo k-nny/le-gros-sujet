a619adeffbca7c3ac155fb3b6b84af5d1d0be453
aina-carpentier <aina.carpentier@etu.univ-smb.fr>
2025-03-24 14:33:25 +0100

    animations

--- src/explosion.js
+++ src/explosion.js
@@ -1,3 +1,16 @@
+animations = [
+    {
+        gif: 'https://media.tenor.com/NJqk_2_eQ40AAAAi/explosion-gif-transparent.gif',
+        sound: 'explosion.mp3'
+    },
+    {
+        gif: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/55b3eef7-e3ab-4675-9f91-3d5353a8cecd/dis0rc5-4093eb20-136a-4e7c-993f-d6366a3f4159.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzU1YjNlZWY3LWUzYWItNDY3NS05ZjkxLTNkNTM1M2E4Y2VjZFwvZGlzMHJjNS00MDkzZWIyMC0xMzZhLTRlN2MtOTkzZi1kNjM2NmEzZjQxNTkuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.gLpP0uvVP2ReAVJfZR8Kz4PFubP27V72skCgYZ1ECjY',
+        sound: 'laser.mp3'
+    },
+
+]
+
+
 document.querySelectorAll('.delete-button').forEach(button => {
     button.addEventListener('click', function() {
         const taskItem = this.closest('li'); // Trouver l'élément de la tâche
@@ -6,15 +19,10 @@ document.querySelectorAll('.delete-button').forEach(button => {
         // Positionner le parent de manière relative (nécessaire pour absolute)
         taskItem.style.position = 'relative';
 
-        // Créer un élément pour l'explosion
-        const explosion = document.createElement('img');
-        explosion.src = 'https://media.tenor.com/NJqk_2_eQ40AAAAi/explosion-gif-transparent.gif';
-        explosion.classList.add('explosion-gif');
-        taskItem.appendChild(explosion);
+        
+        explosion(taskItem, animations)
 
-         // Jouer le son d'explosion
-         const explosionSound = new Audio('explosion.mp3'); // Assurez-vous que ce fichier existe dans votre répertoire
-        explosionSound.play();
+
 
         // Soumettre le formulaire après l'explosion
         setTimeout(() => {
@@ -23,3 +31,17 @@ document.querySelectorAll('.delete-button').forEach(button => {
         }, 600); // Durée de l'animation (0.6s)
     });
 });
+
+function explosion(parent, animations){
+    // Créer un élément pour l'explosion
+    var random = Math.floor(Math.random() * animations.length);
+    anim = animations[random];
+    const explosion = document.createElement('img');
+    explosion.src = anim.gif;
+    explosion.classList.add('explosion-gif');
+    parent.appendChild(explosion);
+
+        // Jouer le son d'explosion
+        const explosionSound = new Audio("assets/" + anim.sound); // Assurez-vous que ce fichier existe dans votre répertoire
+    explosionSound.play();
+}
