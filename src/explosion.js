c61b3a6355703be3a893241c57325461960436fb
aina-carpentier <aina.carpentier@etu.univ-smb.fr>
2025-03-24 15:02:57 +0100

    autre animation

--- src/explosion.js
+++ src/explosion.js
@@ -7,6 +7,10 @@ animations = [
         gif: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/55b3eef7-e3ab-4675-9f91-3d5353a8cecd/dis0rc5-4093eb20-136a-4e7c-993f-d6366a3f4159.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzU1YjNlZWY3LWUzYWItNDY3NS05ZjkxLTNkNTM1M2E4Y2VjZFwvZGlzMHJjNS00MDkzZWIyMC0xMzZhLTRlN2MtOTkzZi1kNjM2NmEzZjQxNTkuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.gLpP0uvVP2ReAVJfZR8Kz4PFubP27V72skCgYZ1ECjY',
         sound: 'laser.mp3'
     },
+    {
+        gif: 'https://i.pinimg.com/originals/2a/39/6e/2a396e7bfb1c6189a00a71a7c3f9ec8e.gif',
+        sound: '8bit-shot.wav'
+    },
 
 ]
 
